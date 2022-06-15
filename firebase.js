c66ac81f6035203e3730f613e85234958d4f03c2
Didi518 <94155950+Didi518@users.noreply.github.com>
2022-06-15 23:04:09 +0200

    page de connexion

--- firebase.js
+++ firebase.js
@@ -7,7 +7,7 @@ import { getStorage } from 'firebase/storage';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: 'keke-tweet.firebaseapp.com',
   projectId: 'keke-tweet',
   storageBucket: 'keke-tweet.appspot.com',
