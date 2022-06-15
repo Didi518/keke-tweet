8e607942eb782eafaa9e6bf0603b6994e1d20cd1
Didi518 <94155950+Didi518@users.noreply.github.com>
2022-06-16 00:39:58 +0200

    session et rendre sidebar & input dynamiques

--- components/Input.js
+++ components/Input.js
@@ -1,31 +1,39 @@
 import { EmojiHappyIcon, PhotographIcon } from '@heroicons/react/outline';
+import { useSession, signOut } from 'next-auth/react';
 
 export default function Input() {
+  const { data: session } = useSession();
+  console.log(session);
   return (
-    <div className="flex border-b border-gray-200 p-3 space-x-3">
-      <img
-        src="http://localhost:3000/_next/image?url=https%3A%2F%2Fpvtistes.net%2Fforum%2Fattachments%2Fcharte-de-pvtistes-net-et-aide-lutilisation-12%2F3089d1540903562-comment-creer-son-avatar-style-manga-20110129191325_yepaland_c57fn8tihamxpyg3euzd9bor4lwjvq06s2k1.jpg&w=128&q=75"
-        alt="img-utilisateur"
-        className="h-11 w-11 rounded-full cursor-pointer hover:brightness-95"
-      ></img>
-      <div className="w-full divide-y divide-gray-200">
-        <div className="">
-          <textarea
-            className="w-full border-none focus:ring-0 text-lg placeholder-gray-700 tracking-wide min-h-[50px] text-gray-700"
-            row="2"
-            placeholder="Quoi de neuf?"
-          ></textarea>
-        </div>
-        <div className="flex items-center justify-between pt-2.5">
-          <div className="flex">
-            <PhotographIcon className="h-10 w-10 hoverEffect p-2 text-sky-500 hover:bg-sky-100" />
-            <EmojiHappyIcon className="h-10 w-10 hoverEffect p-2 text-sky-500 hover:bg-sky-100" />
+    <>
+      {session && (
+        <div className="flex border-b border-gray-200 p-3 space-x-3">
+          <img
+            onClick={signOut}
+            src={session.user.image}
+            alt="img-utilisateur"
+            className="h-11 w-11 rounded-full cursor-pointer hover:brightness-95"
+          ></img>
+          <div className="w-full divide-y divide-gray-200">
+            <div className="">
+              <textarea
+                className="w-full border-none focus:ring-0 text-lg placeholder-gray-700 tracking-wide min-h-[50px] text-gray-700"
+                row="2"
+                placeholder="Quoi de neuf?"
+              ></textarea>
+            </div>
+            <div className="flex items-center justify-between pt-2.5">
+              <div className="flex">
+                <PhotographIcon className="h-10 w-10 hoverEffect p-2 text-sky-500 hover:bg-sky-100" />
+                <EmojiHappyIcon className="h-10 w-10 hoverEffect p-2 text-sky-500 hover:bg-sky-100" />
+              </div>
+              <button className="bg-blue-400 text-white px-4 py-1.5 rounded-full font-bold shadow-md hover:brightness-95 disabled:opacity-50">
+                Tweet
+              </button>
+            </div>
           </div>
-          <button className="bg-blue-400 text-white px-4 py-1.5 rounded-full font-bold shadow-md hover:brightness-95 disabled:opacity-50">
-            Tweet
-          </button>
         </div>
-      </div>
-    </div>
+      )}
+    </>
   );
 }
--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,4 +1,3 @@
-import Image from 'next/image';
 import SidebarMenuItem from './SidebarMenuItem';
 import { HomeIcon } from '@heroicons/react/solid';
 import {
@@ -11,12 +10,15 @@ import {
   InboxIcon,
   UserIcon,
 } from '@heroicons/react/outline';
+import { useSession, signIn, signOut } from 'next-auth/react';
+
 export default function Sidebar() {
+  const { data: session } = useSession();
   return (
     <div className="hidden sm:flex flex-col p-2 xl:items-start fixed h-full xl:ml-24">
       {/* Logo */}
       <div className="hoverEffect p-0 hover:bg-blue-100 xl:px-1">
-        <Image
+        <img
           width="50"
           height="50"
           src="https://help.twitter.com/content/dam/help-twitter/brand/logo.png"
@@ -28,34 +30,51 @@ export default function Sidebar() {
       <div className="mt-4 mb-2.5 xl:items-start">
         <SidebarMenuItem text="Accueil" Icon={HomeIcon} active />
         <SidebarMenuItem text="Explorer" Icon={HashtagIcon} />
-        <SidebarMenuItem text="Notifications" Icon={BellIcon} />
-        <SidebarMenuItem text="Messages" Icon={InboxIcon} />
-        <SidebarMenuItem text="Signets" Icon={BookmarkIcon} />
-        <SidebarMenuItem text="Presse-papier" Icon={ClipboardIcon} />
-        <SidebarMenuItem text="Profil" Icon={UserIcon} />
-        <SidebarMenuItem text="Plus encore" Icon={DotsCircleHorizontalIcon} />
+        {session && (
+          <>
+            <SidebarMenuItem text="Notifications" Icon={BellIcon} />
+            <SidebarMenuItem text="Messages" Icon={InboxIcon} />
+            <SidebarMenuItem text="Signets" Icon={BookmarkIcon} />
+            <SidebarMenuItem text="Presse-papier" Icon={ClipboardIcon} />
+            <SidebarMenuItem text="Profil" Icon={UserIcon} />
+            <SidebarMenuItem
+              text="Plus encore"
+              Icon={DotsCircleHorizontalIcon}
+            />
+          </>
+        )}
       </div>
 
       {/* Bouton */}
-      <button className="bg-blue-400 text-white rounded-full w-56 h-12 font-bold shadow-md hover:brightness-95 text-lg hidden xl:inline">
-        Tweet
-      </button>
+      {session ? (
+        <>
+          <button className="bg-blue-400 text-white rounded-full w-56 h-12 font-bold shadow-md hover:brightness-95 text-lg hidden xl:inline">
+            Tweet
+          </button>
 
-      {/* Mini-profil */}
-      <div className="hoverEffect text-gray-700 flex items-center justify-center xl:justify-start mt-auto">
-        <Image
-          width="50px"
-          height="50px"
-          src="https://pvtistes.net/forum/attachments/charte-de-pvtistes-net-et-aide-lutilisation-12/3089d1540903562-comment-creer-son-avatar-style-manga-20110129191325_yepaland_c57fn8tihamxpyg3euzd9bor4lwjvq06s2k1.jpg"
-          alt="image-utilisateur"
-          className="h-10 w-10 rounded-full xl:mr-2"
-        />
-        <div className="leading-5 hidden xl:inline">
-          <h4 className="font-bold">Kevin Ouali</h4>
-          <p className="text-gray-500">@Didi518</p>
-        </div>
-        <DotsHorizontalIcon className="h-5 xl:ml-8 hidden xl:inline" />
-      </div>
+          {/* Mini-profil */}
+          <div className="hoverEffect text-gray-700 flex items-center justify-center xl:justify-start mt-auto">
+            <img
+              onClick={signOut}
+              src={session.user.image}
+              alt="image-utilisateur"
+              className="h-10 w-10 rounded-full xl:mr-2"
+            />
+            <div className="leading-5 hidden xl:inline">
+              <h4 className="font-bold">{session.user.name}</h4>
+              <p className="text-gray-500">@{session.user.username}</p>
+            </div>
+            <DotsHorizontalIcon className="h-5 xl:ml-8 hidden xl:inline" />
+          </div>
+        </>
+      ) : (
+        <button
+          onClick={signIn}
+          className="bg-blue-400 text-white rounded-full w-36 h-12 font-bold shadow-md hover:brightness-95 text-lg hidden xl:inline"
+        >
+          Connexion
+        </button>
+      )}
     </div>
   );
 }
